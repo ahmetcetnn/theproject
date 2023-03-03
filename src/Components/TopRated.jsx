e9fa54aae4647cf132dd497f3cf69c8720ddddc0
ahmetcetnn <ahmetlaw@outlook.com>
2023-03-03 04:18:20 +0300

    modal popup completed. searchbar created

--- src/Components/TopRated.jsx
+++ src/Components/TopRated.jsx
@@ -2,67 +2,88 @@ import React from 'react'
 import {AiTwotoneStar} from "react-icons/ai"
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { useState,useEffect } from 'react';
-import { Button, Modal } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function TopRated() {
+  const [show, setShow] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-  //modal
-  
+  const API_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key=002f45e7d56066b7503bddca0e16ee67&lanuage=tr";
 
-  
-  const [movies,setMovies] = useState([]);
-  const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=002f45e7d56066b7503bddca0e16ee67&lanuage=tr";
-  
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
-      .then(data => {
+      .then((data) => {
         console.log(data);
         setMovies(data.results);
-      })
+      });
   }, []);
 
+  const handleClose = () => {
+    setShow(false);
+    setSelectedMovie(null);
+  };
 
+  const handleShow = (movie) => {
+    setSelectedMovie(movie);
+    setShow(true);
+  };
 
-
-
-
-
-
-
-return(<div>
-    
-  <div className='toprated'>
-    <h1><AiTwotoneStar/> Toprated Movies</h1>
-    <Splide options={{
-        perPage:4,
-        arrows:false,
-        pagination:false,
-        drag:'free',
-        gap:'3rem',
-        height:"20rem"
-        
-      }}>
-      {movies.map(movie => (
-        <SplideSlide key={movie.id} >
+  return (
+    <div>
+      <div className="toprated">
+        <h1>
+          <AiTwotoneStar /> Toprated Movies
+        </h1>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "3rem",
+            height: "20rem",
+          }}
+        >
+          {movies.map((movie) => (
+            <SplideSlide key={movie.id}>
+              <div className="trsplideimage">
+                <img
+                  id="trsplideimage"
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <button id="trsplidebutton" onClick={() => handleShow(movie)}>
+                  View More
+                </button>
+              </div>
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
      
-                <div className='trsplideimage' >
-                <img id='trsplideimage' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-                <button id='trsplidebutton'   >View More</button>
-              
-                </div>
-               
-                
-             
-        </SplideSlide>
+     
+     
+      <Modal show={show} onHide={handleClose}>
         
-      ))}
-      
-    </Splide>
-    
-  </div>
- 
-  </div>
-)
+        <Modal.Body>
+          {selectedMovie && (
+            <div>
+               <img id='modalimage' src={`https://image.tmdb.org/t/p/w500/${selectedMovie.poster_path}`} alt={selectedMovie.title} />
+              <h2>{selectedMovie.title}</h2>
+              <p>{selectedMovie.overview}</p>
+               <button id='modalbutton' onClick={handleClose}>
+               Close Page
+               </button>
+            </div>
+          )}
+        </Modal.Body>
+        
+      </Modal>
+    </div>
+  );
 }
-
 export default TopRated
