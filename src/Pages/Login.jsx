434fe3de1a65d65774c7ccf066c73365dd666c99
ahmetcetnn <ahmetlaw@outlook.com>
2023-03-04 17:07:01 +0300

    firebase

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,28 +7,37 @@ function Login() {
 
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
+  const [error,setError] =useState(true)
 
 
-
+  const handleLogin = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <div className="general">
   
-      <h1> Giriş Yap</h1>
+     
 
         <div className="section">
-      <div className="login">
-      <button><FcGoogle/> Google ile devam et</button>
-      <button><BsFacebook/> Facebook ile devam et</button>
-      <button><DiApple/> Apple ile devam et</button>
-
-      <br></br>
-      <input type="e-mail" placeholder='E-posta Adresinizi Girin.' value={email} onChange={e => setEmail(e.target.value)}></input>
-      <input type="password" placeholder='Şifrenizi Girin.'value={password} onChange={e => setPassword(e.target.value)}></input>
-      <button disabled={!email || !password} onSubmit> GİRİŞ YAP</button>
-       <p>Google, Apple, veya e-posta ile devam ederek Yekflix'in Hizmet Kullanım Şartlarını 
-        ve <span>Gizlilik Politikasısnı </span> kabul etmiş olursun.</p>
-        <p>Üyeliğin yok mu? <Link to="/register"> Kayıt Ol</Link></p>
+        <div className="login">
+            <form onSubmit={handleLogin}>
+          <button><FcGoogle/> Google ile devam et</button>
+          <button><BsFacebook/> Facebook ile devam et</button>
+          <button><DiApple/> Apple ile devam et</button>
+          <br></br>
+          <input type="e-mail" placeholder='E-posta Adresinizi Girin.' value={email} onChange={e => setEmail(e.target.value)}></input>
+          <input type="password" placeholder='Şifrenizi Girin.'value={password} onChange={e => setPassword(e.target.value)}></input>
+          <button disabled={!email || !password} type="submit"> GİRİŞ YAP</button>
+          </form>
+          
+          
+          
+          
+            {error && <span style={{color:"red"}}>Wrong e-mail or password!</span> } 
+          <p>Google, Apple, veya e-posta ile devam ederek Yekflix'in Hizmet Kullanım Şartlarını 
+            ve <span>Gizlilik Politikasısnı </span> kabul etmiş olursun.</p>
+            <p>Üyeliğin yok mu? <Link to="/register"> Kayıt Ol</Link></p>
       </div>
       </div>
       </div>
