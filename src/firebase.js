1954ad365b0b7648815f5fbff277e8cec348af0f
ahmetcetnn <ahmetlaw@outlook.com>
2023-03-04 23:06:17 +0300

    <

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   storageBucket: "thproject-movie.appspot.com",
   messagingSenderId: "547531881629",
   appId: "1:547531881629:web:3be7e3cc6d5823e551e874",
-  measurementId: "G-H60D3L5LWC"
+  measurementId: "G-H60D3L5LWC""
 };
 
 export const auth= getAuth(app)
