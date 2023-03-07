128314da5c6fa5a8c731488bef89bd6d081c2abf
ahmetcetnn <ahmetlaw@outlook.com>
2023-03-07 16:24:30 +0300

    <

--- src/Extensions/Navbar.jsx
+++ src/Extensions/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import yekflix from "../images/yekflix.png"
 import { Link } from 'react-router-dom'
 import {BiSearchAlt2} from "react-icons/bi"
 function Navbar() {
+  const [searchKey, setSearchKey] =useState("")
+
+  const searchMovie = (e) => {
+  e.preventDefault()
+  }
+
+
   return (
     <div className='navbarüst'>
         
@@ -14,14 +21,24 @@ function Navbar() {
         <li>
             <ul>Home</ul>
             <ul>Series</ul>
-            <ul>Tv Show</ul>
-            <div className='search'>
-            <input type="text" placeholder='Search Movie' className='searchbar'/>
-            <button type='submit' id='submitbtn'>Search <BiSearchAlt2 style={{marginBottom:"5px"}}/></button>
-            </div>
-          
+            <ul>Tv Show</ul>  
         </li>
-        <div className='logreg'>  <Link to ="/login" id='item1' className='navlink'><li>Log In </li></Link> | <Link to ="/register" id='item2' className='navlink'><li> <span>Register</span></li></Link> </div>
+
+        <form onSubmit={searchMovie}>
+        <div className='search'>
+            <input type="text"
+              placeholder='Search Movie'
+              className='searchbar' 
+              onChange={searchKey}/>
+              <button type='submit'
+              id='submitbtn'>Search 
+              <BiSearchAlt2 style={{marginBottom:"5px"}}/>
+              </button>
+            </div>
+        </form>
+        <div className='logreg'>  
+        <Link to ="/login" id='item1' className='navlink'><li>Log In </li></Link> | <Link to ="/register" id='item2' className='navlink'><li> <span>Register</span></li></Link> 
+        </div>
        
        
         </div>
