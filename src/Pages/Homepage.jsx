46bdb43ded0625548186e2e2cb41da03b1a60617
ahmetcetnn <ahmetlaw@outlook.com>
2023-03-11 17:26:06 +0300

    <

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Menu from '../Extensions/Menu'
+import Navbar from '../Extensions/Navbar'
+import MainSection from '../Extensions/MainSection'
+import Latest from "../Components/Latest"
+function Homepage() {
+  return (
+   
+    <div>
+        <Navbar/>
+        <MainSection/>
+        <Latest/>
+        <Menu/>       
+        </div>
+    
+
+  )
+}
+
+export default Homepage
